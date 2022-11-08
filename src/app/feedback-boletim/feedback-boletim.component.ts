e6e769b17d23e7c9bdffb2b5c1d3396b2f7f7f50
Juanpablito1 <87317227+Juanpablito1@users.noreply.github.com>
2022-11-08 16:36:35 -0300

    3 files added

---
+++ src/app/feedback-boletim/feedback-boletim.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback-boletim',
+  templateUrl: './feedback-boletim.component.html',
+  styleUrls: ['./feedback-boletim.component.css']
+})
+export class FeedbackBoletimComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
