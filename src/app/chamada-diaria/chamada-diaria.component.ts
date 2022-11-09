5f6c0fd65e64666cb6f3941774afe7ab21de6854
Murilo-D-Rosa <116669557+Murilo-D-Rosa@users.noreply.github.com>
2022-11-09 11:01:10 -0300

    chamadadiaria

---
+++ src/app/chamada-diaria/chamada-diaria.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chamada-diaria',
+  templateUrl: './chamada-diaria.component.html',
+  styleUrls: ['./chamada-diaria.component.css']
+})
+export class ChamadaDiariaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
