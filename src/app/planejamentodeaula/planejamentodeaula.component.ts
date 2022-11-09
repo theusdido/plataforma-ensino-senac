669ce9cbe0e7b8711dbe7a89daf58594adb5bb58
ruantuktuk <116669418+ruantuktuk@users.noreply.github.com>
2022-11-09 11:29:48 -0300

    3 files added

---
+++ src/app/planejamentodeaula/planejamentodeaula.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planejamentodeaula',
+  templateUrl: './planejamentodeaula.component.html',
+  styleUrls: ['./planejamentodeaula.component.css']
+})
+export class PlanejamentodeaulaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
