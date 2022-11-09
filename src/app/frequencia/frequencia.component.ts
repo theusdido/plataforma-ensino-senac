3e5eef83698be2041f8b1461f6bbdce0d579de12
pedroiv2006 <116113018+pedroiv2006@users.noreply.github.com>
2022-11-09 11:17:02 -0300

    3 files added

---
+++ src/app/frequencia/frequencia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frequencia',
+  templateUrl: './frequencia.component.html',
+  styleUrls: ['./frequencia.component.css']
+})
+export class FrequenciaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
