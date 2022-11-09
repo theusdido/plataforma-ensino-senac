809689730323812b28434514ab6f0266451ab5c9
kaua-Frassetto <116725383+kaua-Frassetto@users.noreply.github.com>
2022-11-09 11:29:25 -0300

    3 files added

---
+++ src/app/justificativa-falta/justificativa-falta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-justificativa-falta',
+  templateUrl: './justificativa-falta.component.html',
+  styleUrls: ['./justificativa-falta.component.css']
+})
+export class JustificativaFaltaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
