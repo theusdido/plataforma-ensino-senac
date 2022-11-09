aad270f5a9f65868b3e95eee6e20e6e5079d806c
Ketulin13 <116669641+Ketulin13@users.noreply.github.com>
2022-11-09 11:29:17 -0300

    3 files added

---
+++ src/app/adicionaratividaes/adicionaratividaes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adicionaratividaes',
+  templateUrl: './adicionaratividaes.component.html',
+  styleUrls: ['./adicionaratividaes.component.css']
+})
+export class AdicionaratividaesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
