abe17f650c6ca7fb7aaa1ba09b02da8ef3c3121a
mequeniques <116669205+mequeniques@users.noreply.github.com>
2022-11-09 11:29:58 -0300

    calendarioescolar

---
+++ src/app/calendarioescolar/calendarioescolar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calendarioescolar',
+  templateUrl: './calendarioescolar.component.html',
+  styleUrls: ['./calendarioescolar.component.css']
+})
+export class CalendarioescolarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
